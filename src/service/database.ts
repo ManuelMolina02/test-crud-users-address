ae53f10faad9f1d09f31c57835aa958727578d79
ManuelMolina02 <manuelanmolina@gmail.com>
2022-02-12 14:40:43 -0300

    adicionando extensoes | organizando arquivos de inicializaçao firebase

--- src/service/database.ts
+++ src/service/database.ts
@@ -1,24 +1,41 @@
+import { firebaseApp } from "./firebase";
 import { getDatabase, ref, set, remove } from "firebase/database";
-import { firebaseApp } from "./ firebase";
 
+//iniciando firebase e banco de dados
 firebaseApp()
 export const db = getDatabase();
 
-interface createUserProps {
+//definindo interface de usuário
+interface UserProps {
   key: string,
   name: string,
-  cpf: number
+  cpf: string
 }
 
-export function createUser({ key, name, cpf }: createUserProps) {
-  set(ref(db, 'users/' + key), {
-    key,
+
+// -> Queries banco de dados
+
+export function setUserCreation({ key, name, cpf }: UserProps) {
+  // validar se dados no form são existentes
+  // se não existir nada apenas pare a execução
+  if (!name || !cpf) {
+    alert('Name or CPF is not defined!')
+    return;
+  }
+
+  set(ref(db, `users/${key}`), {
+    name,
+    cpf
+  })
+}
+
+export function setUserUpdate({ key, name, cpf }) {
+  set(ref(db, `users/${key}`), {
     name,
     cpf
   })
 }
 
 export function deleteUser(key: string) {
-
   remove(ref(db, `users/${key}`))
 }
