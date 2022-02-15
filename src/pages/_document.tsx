a6501dd47dd4e69d9dc1b678c7f20a5c6aed18de
ManuelMolina02 <manuelanmolina@gmail.com>
2022-02-14 23:27:26 -0300

    adicionando novas bibliotecas | configurando arquivos para receber biblioteca na estrutura

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -9,6 +9,7 @@ export default class myDocument extends Document {
           <link rel="shortcut icon" href="/favicon.ico" type="image/ico" />
           <link rel="preconnect" href="https://fonts.googleapis.com" />
           <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@300;400;500;700&family=Ubuntu:wght@400;700&display=swap" rel="stylesheet" />
+          <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossOrigin="anonymous" />
 
         </Head>
         <body>
